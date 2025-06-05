c78cc1357a39ebd815da8cafc84fe795ab3d5fec
dinhan2610 <truongdinhan000@gmail.com>
2025-06-06 01:44:30 +0700

    add BlogPages

--- src/modules/HomeTemplate/index.jsx
+++ src/modules/HomeTemplate/index.jsx
@@ -1,9 +1,8 @@
 import Aboutus from "./About";
 import Appointment from "./Appoinment";
 import Carousel from "./Carousel";
-import Pricing from "./Pricing";
+
 import ShortServices from "./ShortServices";
-import Srvice from "./Srvice";
 import Testimonials from "./Testimonials";
 import WhyChoose from "./Whychoose";
 import WowProvider from "../../components/Wow";
@@ -14,9 +13,7 @@ export default function HomeTemplate() {
         <Carousel />
         <ShortServices />
         <Aboutus />
-        <Srvice />
         <WhyChoose />
-        <Pricing />
         <Testimonials />
         <Appointment />
       </WowProvider>
